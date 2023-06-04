c379622d82a6160f072477f059d8912f1a825d13
Harshit <harshit242016@gmail.com>
2023-06-05 05:19:18 +0530

    Added login page

---
+++ src/components/login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, Alert } from 'antd';
+import axios from 'axios';
+
+const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/authenticate', values);
+      const { token } = response.data;
+      login(token);
+      navigate('/dashboard'); // Redirect to dashboard page
+    } catch (error) {
+      setError('Invalid email or password');
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        style={{ width: 300 }}
+      >
+        <h1 style={{ marginBottom: 24 }}>Login</h1>
+        {error && <Alert message={error} type="error" style={{ marginBottom: 16 }} />}
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input placeholder="Email" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
