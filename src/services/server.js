a3416426d7114eaaf726800aa6e65678b3bd7c5d
Harshit <harshit242016@gmail.com>
2023-06-05 05:20:11 +0530

    Added server.js and mockdata files for custom logging users

---
+++ src/services/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const users = require('../utils/mockdata');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const SECRET_KEY = 'mysecretkey';
+
+app.post('/api/authenticate', (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(u => u.email === email && u.password === password);
+  console.log(user);
+  if (user) {
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY);
+    console.log(token);
+    res.send({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid email or password' });
+  }
+});
+
+app.get('/api/data', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userId = decoded.userId;
+    const user = users.find(u => u.id === userId);
+    res.send({ message: `Welcome ${user.name}! This is your private data.` });
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
+
+app.get('/',(req,res) => {
+    console.log("Your app is running");
+    res.status(200).json({message : "App is running"});
+})
+
+app.listen(5000, () => console.log('Server started on port 5000'));
---
+++ src/utils/mockdata.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: 'User 1', email: 'user1@ex.com', password: 'Test1234' },
+  { id: 2, name: 'User 2', email: 'user2@ex.com', password: 'Test1234' },
+  { id: 3, name: 'User 3', email: 'user3@ex.com', password: 'Test1234' },
+];
+
+module.exports = users;
