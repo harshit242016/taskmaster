ba9cc293547865ec2d62e44caa5dfd17044d6d3e
Harshit <harshit242016@gmail.com>
2023-06-29 12:13:26 +0530

    Changed source in vercel.json file

--- src/services/server.js
+++ src/services/server.js
@@ -40,4 +40,4 @@ app.get('/',(req,res) => {
     res.status(200).json({message : "App is running"});
 })
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(process.env.PORT || 5000, () => console.log('Server started on port 5000'));
